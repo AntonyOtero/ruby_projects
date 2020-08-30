2aa71744920433cac3cc51a8c39055fe8e185eff
Railon Acosta <cagedtreestechnology@gmail.com>
2020-08-30 14:41:02 -0400

    notes

---
+++ bubble_sort/bubble-sort.rb
@@ -0,0 +1,23 @@
+
+
+
+procedure bubbleSort(A : list of sortable items)
+n := length(A)
+repeat
+    swapped := false
+    for i := 1 to n-1 inclusive do
+        #/* if this pair is out of order */
+        if A[i-1] > A[i] then
+           # /* swap them and remember something changed */
+            swap(A[i-1], A[i])
+            swapped := true
+        end if
+    end 
+until not swapped
+end procedure
+
+
+
+procedure([6,4,5,3,6,19])
+
+
