eafcb9e153187a8f609eb8f5721d49a74cba9f2b
= <=>
2020-09-04 22:01:18 -0400

    Complete class structure

---
+++ hangman/lib/hangman.rb
@@ -0,0 +1,80 @@
+class Hangman
+  attr_reader :selected_word
+  attr_reader :graphics
+  attr_reader :attempts
+  attr_reader :letters_display
+  attr_reader :guess_display
+  
+  def initialize
+    @dictionary = File.read("5desk.txt")
+    @graphics = File.read("graphics.txt")
+    @selected_word = get_word
+    @letters_display = "\na b c d e f g h i j k l m n o p q r s t u v w x y z\n\n"
+    @guess_display = generate_guess(@selected_word)
+    @attempts = 6
+  end
+
+  def get_word
+    dictionary_arr = @dictionary.split("\r\n")
+    words_in_range = dictionary_arr.select {|word| word.length.between?(5, 12)}
+    return words_in_range.sample
+  end
+
+  def generate_guess(word)
+    letter_arr = word.split("")
+    hidden_arr = letter_arr.map {|letter| "_"}
+
+    # "\n_ _ _ _ _ _ _ _ _\n\n"
+    "\n" + hidden_arr.join(" ") + "\n\n"
+  end
+
+  def update_display(guess)
+    if @attempts != 1 then
+      # GUESS DISPLAY
+      if @selected_word.include? guess then
+        selected_arr = @selected_word.split("")
+        selected_arr.each_with_index do |letter, index|
+          if letter == guess then
+            @guess_display[(index * 2) + 1] = guess
+          end
+        end
+      else
+        @attempts -= 1
+      end
+      # LETTERS DISPLAY
+      if @letters_display.include? guess then
+        @letters_display[@letters_display.index(guess)] = "-"
+      end
+      
+      play
+    elsif @attempts == 1
+      system("clear")
+      puts "YOU LOSE!"
+    end
+  end
+
+  def make_guess
+    # Does guess equal selected word?
+    guess = ""
+    loop do
+      print "Guess: "
+      guess = gets.chomp
+      if guess.length == 1
+        break
+      end
+    end
+
+    update_display(guess)
+  end
+  
+  def play
+    print @letters_display
+    puts "{Attempts left: #{@attempts}, Word: #{@selected_word}}"
+    puts @graphics
+    print @guess_display
+    print make_guess
+  end
+end
+
+new_game = Hangman.new
+new_game.play
