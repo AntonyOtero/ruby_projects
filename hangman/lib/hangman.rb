438d57da6e969f23d96633fd324c611ed87e3ce2
= <=>
2020-09-09 14:19:57 -0400

    Add | save/load functionality

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -6,41 +6,52 @@
 =end
 
 require 'fileutils'
+require 'json'
+
+if not Dir.exist? "local" then
+  FileUtils.mkdir "local"
+  FileUtils.mkdir "local/saves"
+end
+
+def load
+  data = JSON.parse(File.read "local/saves/saved.txt")
+  Hangman.new data["selected_word"], data["letters_ui"], data["guess_ui"], data["attempts"]
+end
+
+def generate_word
+  dictionary_arr = File.read("5desk.txt").split("\r\n")
+  words_in_range = dictionary_arr.select {|word| word.length.between?(5, 12)}
+  return words_in_range.sample.downcase
+end
+
+def generate_guess_ui(word)
+  letter_arr = word.split("")
+  hidden_arr = letter_arr.map {|letter| "_"}
+  "\n" + hidden_arr.join(" ") + "\n\n"
+end
 
 class Hangman
-  # attr_reader :selected_word
-  # attr_reader :graphics
-  # attr_reader :attempts
-  # attr_reader :letters_display
-  # attr_reader :guess_display
-  
-  
-  def initialize
-    if Dir.exist?("local") then
-      FileUtils.rm_rf "local"
-      FileUtils.mkdir "local"
-      FileUtils.mkdir "local/saves"
-      FileUtils.copy "graphics.txt", "local/graphics_inst.txt"
-    end
 
-    @dictionary = "5desk.txt"
+  def initialize selected_word=generate_word, letters_ui="\na b c d e f g h i j k l m n o p q r s t u v w x y z\n\n", guess_ui=generate_guess_ui(selected_word), attempts=6
+
     @graphics = "local/graphics_inst.txt"
-    @selected_word = generate_word
-    @letters_ui = "\na b c d e f g h i j k l m n o p q r s t u v w x y z\n\n"
-    @guess_ui = generate_guess_ui(@selected_word)
-    @attempts = 6
+    @selected_word = selected_word
+    @letters_ui = letters_ui
+    @guess_ui = guess_ui
+    @attempts = attempts
   end
-
-  def generate_word
-    dictionary_arr = File.read(@dictionary).split("\r\n")
-    words_in_range = dictionary_arr.select {|word| word.length.between?(5, 12)}
-    return words_in_range.sample.downcase
-  end
-
-  def generate_guess_ui(word)
-    letter_arr = word.split("")
-    hidden_arr = letter_arr.map {|letter| "_"}
-    "\n" + hidden_arr.join(" ") + "\n\n"
+  
+  attr_reader :selected_word
+  attr_reader :graphics
+  attr_reader :attempts
+  attr_reader :letters_display
+  attr_reader :guess_display
+  
+  def save
+    FileUtils.touch "local/saves/saved.txt"
+    json = JSON.dump({"selected_word" => @selected_word, "letters_ui" => @letters_ui, "guess_ui" => @guess_ui, "attempts" => @attempts})
+    File.write "local/saves/saved.txt", json
+    puts "SAVED GAME."
   end
 
   def update_guess_ui(guess)
@@ -58,6 +69,8 @@ class Hangman
     if not @selected_word.include? guess then
       @attempts -= 1
       case @attempts
+      when 6
+        File.write(@graphics, ["____ ", "|  | ", "|    ", "|    ", "|    "].join("\n"))
       when 5
         File.write(@graphics, ["____ ", "|  | ", "|  O ", "|    ", "|    "].join("\n"))
       when 4
@@ -94,11 +107,11 @@ class Hangman
     loop do
       print "Guess a letter: "
       guess = gets.chomp.downcase
-      if guess.length == 1
+      if guess.length == 1 or guess == ":save"
         break
       end
     end
-    update_ui(guess)
+    (guess == ":save") ? save : update_ui(guess)
   end
   
   def get_graphics
@@ -127,5 +140,22 @@ class Hangman
   end
 end
 
-new_game = Hangman.new
-new_game.play
+selected_option = ""
+loop do
+  system("clear")
+  print "Start a new game or load saved game (new/load): "
+  selected_option = gets
+  if selected_option == "new\n" or selected_option == "load\n" then
+    break
+  end
+end
+
+case selected_option
+when "new\n"
+  FileUtils.copy "graphics.txt", "local/graphics_inst.txt"
+  new_game = Hangman.new
+  new_game.play
+when "load\n"
+  saved_game = load
+  saved_game.play
+end
